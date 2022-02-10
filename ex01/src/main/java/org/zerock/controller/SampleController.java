7c06d2923ebcb339d9a08abe2c7dbbdfd126de67
song <sgh83037@naver.com>
2022-02-11 01:06:37 +0900

    spring web project 05,06

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,7 +1,22 @@
 package org.zerock.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zerock.domain.SampleDTO;
+import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -10,5 +25,81 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class SampleController {
 
+	@RequestMapping("")
+	public void basic() {
+		log.info("basic...............");
+	}
+	
+	@RequestMapping(value = "/basic", method = {RequestMethod.GET, RequestMethod.POST})
+	public void basicGet() {
+		log.info("basic get................");
+	}
+	
+	@GetMapping("/basicOnlyGet")
+	public void basicGet2() {
+		log.info("basic get only get................");
+	}
+	
+	
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {  //필요한 파라미터 호출시 URL 뒤에 추가해서 호출 ->자동으로 타입을 변환해서 처리됨
+	
+		log.info(""+dto);
+		return "ex01";
+	}
+	
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+		log.info("name"+name);		
+		log.info("age"+age);
+		return "ex02";
+	}
+	
+	@GetMapping("/ex02List")
+	public String ex02List(@RequestParam("ids")ArrayList<String> ids) {
+		
+		log.info("ids : "+ids);
+		return "ex02List";
+	}
 
+	@GetMapping("/ex02Array")
+	public String ex02Array(@RequestParam("ids")String[] ids) {
+		
+		log.info("array ids : "+ Arrays.toString(ids));
+		return"ex02Array";
+	}
+	
+	@GetMapping("/ex02Bean")
+	public String ex02Bean(SampleDTOList list) {
+		
+		log.info("list dtos : " +list);
+		return "ex02Bean";
+	}
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+	}
+	
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		
+		log.info("todo" + todo);
+		return "ex03";
+	}
+	
+	@GetMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("serverTime", new java.util.Date());
+		return "home";
+	}
+	
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		
+		log.info("dto" + dto);
+		log.info("page" + page);
+		return "ex04";
+	}
 }
